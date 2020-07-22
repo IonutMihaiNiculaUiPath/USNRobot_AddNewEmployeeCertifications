a6041fd6c56ee820f4309dc8c732a4625729de25
IonutMihaiNiculaUiPath <ionut.nicula@uipath.com>
2020-07-22 18:14:47 +0300

    updated poc email

--- getsubmissions.ps1
+++ getsubmissions.ps1
@@ -1,2 +1,2 @@
-$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-15 10:34:54"}';
+$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-22 06:03:17"}';
 return $var.content;
