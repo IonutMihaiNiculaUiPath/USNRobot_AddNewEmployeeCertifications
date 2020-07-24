4127b17a85136d3c1d102fb8b1717227cfd30f5f
IonutMihaiNiculaUiPath <ionut.nicula@uipath.com>
2020-07-24 15:14:10 +0300

    small updates

--- getsubmissions.ps1
+++ getsubmissions.ps1
@@ -1,2 +1,2 @@
-$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-22 06:03:17"}';
+$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-22 01:14:48"}';
 return $var.content;
