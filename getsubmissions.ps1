bed1dfac73366dfaebaecfb256954dfe00d39d76
IonutMihaiNiculaUiPath <ionut.nicula@uipath.com>
2020-07-28 13:40:11 +0300

    fixed bug on candidate applying the 2nd time to the same certification

--- getsubmissions.ps1
+++ getsubmissions.ps1
@@ -1,2 +1,2 @@
-$var = curl -method GET 'https://eu-api.jotform.com/form/202092041237342/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-27 10:44:00"}';
+$var = curl -method GET 'https://eu-api.jotform.com/form/202092041237342/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-28 01:29:21"}';
 return $var.content;
