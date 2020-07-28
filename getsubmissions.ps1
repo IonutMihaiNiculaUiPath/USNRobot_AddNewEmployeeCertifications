102f5f2b45fcde4d8d1d43a571ab2146bba1e2bc
IonutMihaiNiculaUiPath <ionut.nicula@uipath.com>
2020-07-28 10:22:37 +0300

    added log for bio

--- getsubmissions.ps1
+++ getsubmissions.ps1
@@ -1,2 +1,2 @@
-$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-24 04:14:07"}';
+$var = curl -method GET 'https://eu-api.jotform.com/form/201971675150354/submissions?apiKey=690e9e362745b963dd630dc46e5c870b&filter={"created_at:gt":"2020-07-26 02:47:43"}';
 return $var.content;
